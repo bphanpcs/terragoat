7588c935d538eb64610361498622ae37279f597e
Binh Phan <92009631+bphanpcs@users.noreply.github.com>
2022-08-25 10:30:39 -0700

    Update s3.tf
    Change financials bucket name

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_object" "data_object" {
   })
 }
 
-resource "aws_s3_bucket" "financials" {
+resource "aws_s3_bucket" "financials2" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
